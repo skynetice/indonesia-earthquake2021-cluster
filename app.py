23a59532e3596704336df0a5546a7ed4d718406a
skynetice <54790583+skynetice@users.noreply.github.com>
2021-12-11 21:50:13 +0700

    initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from multiapp import MultiApp
+from pages import page1, klaster_lokasi, klaster_mag
+
+app = MultiApp()
+
+# Add all your application here
+app.add_app("Titik gempa", page1.app)
+app.add_app("Klaster Lokasi", klaster_lokasi.app)
+app.add_app("Klaster Magnitudo", klaster_mag.app)
+
+
+# The main app
+app.run()
