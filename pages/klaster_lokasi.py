7b678916e3fe1c9a8c27139b3333a5a736f74bd4
Arnold-Prajna <84566545+Arnold-Prajna@users.noreply.github.com>
2021-12-12 19:09:06 +0700

    Update klaster_lokasi.py

--- pages/klaster_lokasi.py
+++ pages/klaster_lokasi.py
@@ -17,7 +17,27 @@ def app():
 
     Algoritma ini dipilih karena menghasilkan dapat menghasilkan klaster yang cukup baik.
     """)
+    ###############################################################################
+    code = '''
+    X = data_cluster
 
+    db = DBSCAN(eps=2.5, min_samples=55).fit(X)
+    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+    core_samples_mask[db.core_sample_indices_] = True
+    labels = db.labels_
+
+    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+    n_noise_ = list(labels).count(-1)
+
+    clusters = labels
+    colors = ['royalblue', 'maroon', 'forestgreen', 'mediumorchid', 'tan', 'deeppink', 'olive', 'goldenrod', 'lightcyan', 'navy', 'black']
+    vectorizer = np.vectorize(lambda x: colors[x % len(colors)])
+    plt.figure(figsize=(12,6))
+    plt.scatter(X[:,0], X[:,1], c=vectorizer(clusters))'''
+    
+    st.code(code, language='python')
+    ###############################################################################
+    
     data = pd.read_csv('data_clean.csv')
 
     df = data.drop(data[data['M'] < 4].index)
