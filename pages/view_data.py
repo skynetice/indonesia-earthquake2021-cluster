b9a0af3144df5984964e40e6d826efad8b4f920e
rhayhan akbar <akb.rhayhan@gmail.com>
2021-12-12 11:11:11 +0700

    Fix bad repo

--- pages/view_data.py
+++ pages/view_data.py
@@ -11,7 +11,7 @@ from sklearn.cluster import KMeans
 
 def app():
     st.write("""
-    #Klasterisasi Gempa Bumi di Indonesia Selama 2021
+    # Klasterisasi Gempa Bumi di Indonesia Selama 2021
 
     Indonesia, terletak tepat di atas cincin api dan pertemuan tiga lempeng tektonik besar yaitu lempeng Eurasia, Indo-Australia, dan Pasifik, menjadikan negara ini rawan bencana alam, khususnya gempa bumi.
     Berdasarkan data USGS (2016), sekitar 90% gempa bumi yang terjadi di dunia termasuk gempa terbesar terjadi di sepanjang cincin api.
@@ -27,9 +27,41 @@ def app():
     
     """)
 
+    st.subheader('Pengumpulan data')
+
+    st.write("""
+    Data yang digunakan dalam proyek ini merupakan data gempa bumi yang terjadi di Indonesia selama tahun 2021 yang diambil dari situs BMKG.
+
+    adapun bentuk data yang diambil adalah sebagai berikut :
+    """)
+
+    data_old = pd.read_csv('data.csv', sep=';')
+
+    
+
     data = pd.read_csv('data_clean.csv')
 
-    st.bar_chart(data['M'])
+    st.dataframe(data_old)
+
+    st.markdown('Data diatas merupakan _Raw_ data atau data mentah.')
+
+    st.write("""
+    Selanjutnya dilakukan preprocessing pada data sehingga data dapat diolah pada proses klasterisasi.
+    """)
+
+    st.subheader('Pengenalan data')
+
+    st.bar_chart(data['M'].value_counts())
+    st.caption('intensitas magnitudo gempa bumi yang terjadi di indonesia')
+
+    st.write("""
+    Data yang diperolah melalui situs BMKG merupakan data gempa bumi yang terjadi di indonesia selama tahun 2021 yang tercatat pada sistem. Namun, dari keseluruhan data gempa yang diperoleh tidak semua memiliki nilai yang signifikan.
+    menurut USSG[1] gempa bumi dapat yang dapat dirasakan oleh manusia berkisar pada magnitudo lebih dari 4 skala richter. Artinya gempa yang memiliki magnitudo lebih kecil dari 4 tidak memiliki nilai yang signifikan sehingga dapat kita abaikan. 
+    """)
+
+    df = data.drop(data[data['M'] < 4].index)
+    df.reset_index(drop=True)
+    st.bar_chart(df['M'].value_counts())
 
     
     st.pyplot(plt)
